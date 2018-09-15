103260f6a7f96a053f8354267cff8f323a376cf0
Muhammad Afiq <afiqon@gmail.com>
2018-09-16 01:43:17 +0800

    tutorial 022 minit 9.49

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb://afiqon:fahima98@ds237072.mlab.com:37072/nodetutorial'
+    'mongodb://afiqon:fahima98@ds237072.mlab.com:37072/nodetutorial',
+    secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -16,7 +21,11 @@ mongoose
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
